9d2b3783d7ab95fb3601290f79688346bc134d52
Diego <diegosabinogomes@gmail.com>
2023-08-30 10:15:48 -0300

    Descrição das modificações

--- main.js
+++ main.js
@@ -1,30 +1,66 @@
 // Objetivo: arquivo principal do projeto
 // Data: 01/07/2021
 // Autor: Lucas Feliciano da Silva
-
-const catalogo = [{
+// 
+const catalogo = [{ 
     id: 1,
-    nome: 'Casaco Branco',
+    nome: 'Camisa Larga com Bolsos',
     marca: 'Zara',
-    preco: 200,
+    preco: 70,
     nomeArquivoImagem: 'product-1.jpg',
-},
-{
-    id: 2,
-    nome: 'Sobretudo Azul Marinho',
-    marca: 'Vercace',
-    preco: 700,
+    feminino: false, },
+    
+    { id: 2,
+    nome: 'Casaco Reto com Lã',
+    marca: 'Zara',
+    preco: 85,
     nomeArquivoImagem: 'product-2.jpg',
-},
-{
-    id: 3,
-    nome: 'Calca Azul Marinho',
-    marca: 'Disel',
-    preco: 350,
+    feminino: true, },
+    
+    { id: 3,
+    nome: 'Jaqueta com Efeito Camurça',
+    marca: 'Zara',
+    preco: 60,
     nomeArquivoImagem: 'product-3.jpg',
-},
-];
-// faz repetir o codigo para cada produto do catalogo 
+    feminino: false, },
+    
+    { id: 4, 
+    nome: 'Sobretudo em Mescla de Lã',
+    marca: 'Zara',
+    preco: 160,
+    nomeArquivoImagem: 'product-4.jpg',
+    feminino: false, },
+    
+    { id: 5,
+    nome: 'Camisa Larga Acolchoada de Veludo Cotelê',
+    marca: 'Zara',
+    preco: 110,
+    nomeArquivoImagem: 'product-5.jpg', 
+    feminino: false, },
+    
+    { id: 6,
+    nome: 'Casaco de Lã com Botões',
+    marca: 'Zara',
+    preco: 170, 
+    nomeArquivoImagem: 'product-6.jpg', 
+    feminino: true, },
+    
+    { id: 7,
+    nome: 'Casaco com Botões',
+    marca: 'Zara',
+    preco: 75,
+    nomeArquivoImagem: 'product-7.jpg',
+    feminino: true, }, 
+    
+    { id: 8,
+    nome: 'Colete Comprido com Cinto',
+    marca: 'Zara',
+    preco: 88, 
+    nomeArquivoImagem: 'product-8.jpg',
+    feminino: true, }];
+
+    
+// faz repetir o codigo para cada produto do catalogo teste
 for(const produtoCatalogo of catalogo) {
     const cartaoProduto = `<div id="produto1">
 <img
